1ce3334f9131633871c9cdc5efffb2dae4ed578b
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-14 13:45:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+  if (s == "") return "";
+  s = s.trim();
+  let i = 0, str = "";
+  while (i <= s.length - 1) {
+    if(s[i] == " ") return str;
+    str += s[i];
+    i++;
+  }
+  return str;
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
